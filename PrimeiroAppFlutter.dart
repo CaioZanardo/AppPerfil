9e79cd378cd1155210b2c351c4601021cd8f906f
CaioZanardo <caio_zanardo@hotmail.com>
2023-08-14 23:35:20 -0300

    PrimeiroAppFlutter

---
+++ PrimeiroAppFlutter.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Colors.blue),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Meu Perfil"),
+          centerTitle: true,
+        ),
+        body: Container(
+          color: Colors.white,
+          child: Center(
+            child: Text(
+              "Nome: Caio Zanardo \n\nFormação: \nCursando Sistemas para internet - FIAP \n\nExperiencia: Analista SRE / Middleware \nAdministração servers Weblogic, Linux, K8s, gerenciamento de incidentes de falha, sintomas, solução de problemas, remediação, causa raiz e monitoramento.",
+              style: TextStyle(
+                fontSize: 22,
+                color: Colors.blue,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+                decoration: TextDecoration.underline,
+                //decoration: TextDecoration.overline,
+                decorationColor: Colors.red,
+                decorationStyle: TextDecorationStyle.dashed,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
